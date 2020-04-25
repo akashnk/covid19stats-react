7232bfc5fe5f02aa52412b8dbbe4ef6afa2ea5fc
Akash Naik <skydockan@gmail.com>
2020-04-25 15:54:53 +0530

    added testing data to table

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Today from "./Today";
 import Dchart from "./Dchart";
 import "./App.css";
 import {TableContext} from './TableContext';
+import {STATES,pop} from './constants';
 
 
 // import Utils from "./Utils";
@@ -30,19 +31,19 @@ function App() {
   const [timeseries,setTimeseries] = useState([]);
    const [graphOption, setGraphOption] = useState(1);
    const [timeseriesMode, setTimeseriesMode] = useState(true)
-    const [timeMode, setTimeMode] = useState(14);
+    const [timeMode, setTimeMode] = useState([]);
 
 const [logMode, setLogMode] = useState(false);
  const [activeStateCode, setActiveStateCode] = useState('TT');
  const [cases,setCase] =useState('totalconfirmed');
-  // const [testsData, setTestsData] = useState({});
-  const [conf,setConf] =useState({});
+  const [testData, setTestData] = useState([]);
+  const [totalTests,setTotaltests] =useState([]);
 
   //const apiURL1 = 'https://corona.lmao.ninja/v2/countries';
   const apiURL1 = 'https://api.covid19india.org/state_district_wise.json';
   const apiURL2 = 'https://api.covid19india.org/data.json';
    const apiURL3 = 'https://api.covid19india.org/states_daily.json';
-  //  const apiURL4 = 'https://api.covid19india.org/state_test_data.json';
+   const apiURL4 = 'https://api.covid19india.org/state_test_data.json';
 //   const apiURL4
  const context = useContext(TableContext);
 // console.log(context.statecodes);
@@ -57,24 +58,31 @@ useEffect(() => {
         {data},
 
        {data: statesDailyResponse},
-      //  {data: stateTestData}
+      {data: stateTestData}
      ] =
      await Promise.all([
       axios.get(apiURL1),
       axios.get(apiURL2),
-     axios.get(apiURL3)
-    //  axios.get(apiURL4)
+     axios.get(apiURL3),
+    axios.get(apiURL4),
    ]);
 
        //console.log(resone.data);
        setDistrictWiseData(resone.data);
        setData(data.statewise);
+
        // setStatesDailyResponse(resthree.data);
        const ts = parseStateTimeseries(statesDailyResponse);
        ts['TT'] = preprocessTimeseries(data.cases_time_series); // TT -> India
        setTimeseries(ts);
-      //  const testData = stateTestData.states_tested_data.reverse();
-    //  const totalTest = data.tested[data.tested.length - 1];
+       setTestData(stateTestData.states_tested_data.reverse());
+       setTotaltests(data.tested[data.tested.length-1])
+      //  testData.push({
+      //   updatedon: totalTests.updatetimestamp,
+      //   totaltested: totalTests.totalindividualstested,
+      //   source: totalTests.source,
+      //   state: 'Total',
+      // });
     //  testData.push({
       //  updatedon: totalTest.updatetimestamp.split(' ')[0],
       //  totaltested: totalTest.totalindividualstested,
@@ -82,7 +90,7 @@ useEffect(() => {
       //  state: 'Total', // India
     //  });
         // setStateTestData(testData);
-       // setTestsData(resthree.data.states_tested_data);
+       
        setFetched(true);
 
     } catch (err) {
@@ -103,6 +111,62 @@ useEffect(() => {
 const fdata = data.filter(d => d.confirmed > 0);
 const kdata = fdata.filter(d => d.state !== "Total");
 const gdata = kdata.sort((a, b) => (b.confirmed - a.confirmed));
+// const testkData = testData.reverse();
+// const totalTest = data.tested[data.tested.length - 1];
+// var arrayv=[];
+//  for (var i=0;i < STATES.length; i++)
+//  {
+//    var st = [];
+//    var j = [];
+// st = Object.values(STATES)[i];
+// j = testData.find(d => d.state === st);
+
+const filtTest=testData.filter((v,i,a)=> a.findIndex(t=>(t.state===v.state && t.totaltested>0))===i)
+// console.log(gdata.map((, i) => Object.assign({}, item, filtTest[i]));
+// const ddata= [...gdata,...filtTest,]
+// let set = new Set();
+// let unionArray = ddata.filter(item => {
+//   if (!set.has(item.state)) {
+//     set.add(item.state);
+//     return true;
+//   }
+//   return false;
+// }, set);
+
+// function mergeArrayObjects(arr1,arr2){
+//   return arr1.map((item,i)=>{
+//      if(item.state === arr2[i].state){
+//          //merging two objects
+//        return Object.assign({},item,arr2[i])
+//      }
+//   })
+// }
+// useEffect(()=>{
+// setTimeMode(mergeArrayObjects(gdata,filtTest));
+// }
+// ,[gdata,filtTest])
+console.log(pop);
+
+let merged = [];
+
+ for(let i=0; i<gdata.length; i++) {
+// gdata.forEach(function(i) {
+  merged.push({
+   ...gdata[i], 
+   ...(filtTest.find((itmInner) => itmInner.state === gdata[i].state))}
+  );
+}
+
+console.log(merged);
+// console.log(testData)
+// arrayv.push(j)
+
+
+//  }
+// console.log(arrayv)
+// var st = []
+// st=Object.values(STATES)[2];
+// console.log(st)
 
 const handleChange = e => {
    // console.log(e.target.value);
@@ -113,7 +177,7 @@ const handleChange = e => {
     setLogMode(e.target.value);
   };
 
- 
+
 // console.log(JSON.stringify(timeseries["TT"])
 // console.log(districtWiseData);
   const columns = useMemo(
@@ -204,6 +268,28 @@ const handleChange = e => {
           </div>)}
       },
 
+      {
+        Header: "Total Tested",
+        accessor: "totaltested"
+        // width: 80
+      },
+
+      {
+        Header: "Positivity %",
+        accessor: (row) => {
+          return( ((row.positive/row.totaltested)*100).toFixed(2))
+        }
+        // width: 80
+      },
+      {
+        Header: "Tests per million",
+        accessor: (row) => {
+          return((row.totaltested/pop[row.statecode]*1000000).toFixed(0))
+        }
+        // width: 80
+      }
+      
+
 
 
         ],
@@ -255,7 +341,7 @@ const handleChange = e => {
 
         <div>
 
-      {fetched && <Table columns={columns} data={gdata}
+      {fetched && <Table columns={columns} data={merged}
             districtWiseData={districtWiseData}
             totaldata={timeseries}
       />}
