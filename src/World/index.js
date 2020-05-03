899e052f74999e165029fddaf0a68248256aff8d
Akash Naik <skydockan@gmail.com>
2020-05-03 06:56:00 +0530

    switched to grid layout in world chart and table

--- src/World/index.js
+++ src/World/index.js
@@ -1,5 +1,8 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import Switch from '@material-ui/core/Switch';
@@ -11,10 +14,54 @@ import Worldtable from './worldtable';
 import Update from './Update';
 import Worldchart from './worldchart';
 
+
+const theme = createMuiTheme({
+
+  typography: {
+    "fontSize": 12.5,
+    variantMapping: {
+      
+      h1: 'h3',
+      h2: 'h4',
+      h3: 'h5',
+      h4: 'h6',
+      h5: 'h7',
+      h6: 'h8',
+      subtitle1: 'h2',
+      subtitle2: 'h2',
+      body1: 'span',
+      body2: 'span',
+    },
+  },
+
+});
+
 const styles = createStyles({
-    formControlLabel: { fontSize: '0.6rem', 
-    '& label': { fontSize: '0.6rem' } }
-  });
+formControlLabel: { fontSize: '0.6rem', 
+'& label': { fontSize: '0.6rem' } }
+});
+
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+root: {
+  flexGrow: 1,
+},
+paper: {
+  padding: theme.spacing(0.2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+},
+menuButton: {
+  marginRight: theme.spacing(2),
+},
+title: {
+  flexGrow: 1,
+},
+}));
+
 
 export default function World() {
     const [datax, setDatax] = useState([]);
@@ -81,11 +128,20 @@ const handleChange = e => {
      setDaysC(e.target.value);
    };
 
+   const classes = useStyles();
     return (
 
         <>
-    
+        <ThemeProvider theme={theme}>
+        <div className={classes.root}>
+    <Grid container spacing={1}>
+    <Grid item xs={12} >
+<Paper className={classes.paper}>
         {fetched &&  <Update dataw={dataw} />}
+        </Paper>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+        <Paper className={classes.paper}>
         <RadioGroup row aria-label="case" name="case" value={value} onChange={handleChange}>
          <FormControlLabel value="confirmed" control={<Radio />} label="Confirmed" />
     <FormControlLabel value="deaths" control={<Radio />} label="Deaths" />
@@ -110,8 +166,16 @@ daysC ={daysC}
       
          
     </RadioGroup>
+    </Paper>
+    </Grid>
+    <Grid item xs={12} sm={12} md={6}>
+    <Paper className={classes.paper}>
        {fetched &&  <Worldtable rows={datax}/>}
-
+       </Paper>
+</Grid>
+</Grid>
+</div>
+</ThemeProvider>
         </>
     );
   }
