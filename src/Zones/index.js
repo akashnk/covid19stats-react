601ea24f1fbd7d19a159d66e63d1cfaf251b7f8e
Akash Naik <skydockan@gmail.com>
2020-05-06 19:35:41 +0530

    add map selection options

--- src/Zones/index.js
+++ src/Zones/index.js
@@ -1,205 +1,54 @@
-import React,{useState} from 'react';
-import ReactDOM from "react-dom";
-import ReactTooltip from "react-tooltip";
-import "./styles.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+// import NativeSelect from '@material-ui/core/NativeSelect';
+import Zone from './zone'
 
-// import {Stage} from './Stage';
-// import ZoomContainer from './ZoomContainer';
-// import { geoMercator, geoPath } from "d3-geo";
-//  import {  min, max,scaleLinear} from "d3";
-
-// const world = require("./indiafinal.json")
-// const data = require("./zones.json")
-// console.log(data)
-// var result = arrOfObj.map(function(el) {
-//     var o = Object.assign({}, el);
-//     o.isActive = true;
-//     return o;
-//   })
-// console.log(world.features)
-// function Zones() {
-//     const mercator = geoMercator().scale(800).translate([-800,600])
-//     const project = geoPath().projection(mercator).pointRadius(1);
-//     const minProp = min(world.features, feature => feature.properties.statecode);
-//     const maxProp = max(world.features, feature => feature.properties.statecode);
-//     const colorScale = scaleLinear()
-//       .domain([minProp, maxProp])
-//       .range(["blue", "red"]);
-//     return (
-//         <>
-//         <h2>India Zones</h2>
-//       <Stage width="100%" height="800">
-//         <ZoomContainer>
-//           {world.features.map((feature, index) => {
-//             const d = project(feature)
-//             return (
-//               <path
-//                 key={index}
-//                 d={d}
-//                 fill={colorScale}
-//                 stroke="#666666"
-//                 strokeWidth={0.25}
-//               />
-//             )
-//           })}
-//         </ZoomContainer>
-//       </Stage>
-//       </>
-//     )
-//   }
-import {
-    ComposableMap,
-    ZoomableGroup,
-    Geographies,
-    Geography,
-    Graticule,
-  } from "react-simple-maps"
-  import { scaleLinear,scaleOrdinal } from "d3-scale"
-  
-  const wrapperStyles = {
-    width: "100%",
-    maxWidth: 980,
-    margin: "0 auto",
-  }
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
   
 
- const datax = require("./zones.json")
- const data =datax.zones
- const INDIA_TOPO_JSON = require('./indiatop.json');
+export default function Zones() {
+    const classes = useStyles();
+    const [maped, setMaped] = React.useState(10);
 
- 
+    const handleChange = (event) => {
+      setMaped(event.target.value);
+    };
 
-  const minValue = 5 // based on the data array above
-  const maxValue = 16 // based on the data array above
-  
-  const minColor = "#CFD8DC"
-  const maxColor = "#37474F"
-  
-  const customScale = scaleLinear()
-    .domain([minValue,maxValue])
-    .range([minColor,maxColor])
-
-    const colorScale = scaleOrdinal()
-    .domain(['#D21C1C','#E98305','#2B7336'])
-    .range(['#2B7336','#D21C1C','#E98305']);
-
-
-  
-  function Zones() {
-    const [content, setTooltipContent] = useState("");
-
-    
-      return (
+    return(
         <>
-        <div style={wrapperStyles}>
-          <ComposableMap
-            projection="geoMercator"
-            projectionConfig={{
-            //   rotate: [-20.0, -52.0, 0],
-            
-            
-              scale: 800
-            }}
-            width={980}
-            height={651}
-            // style={{
-            //   width: "100%",
-            //   height: "auto",
-            // }}
-            >
-            <ZoomableGroup data-tip="" center={[100, 23]} >
-              {/* <Geographies geography="./indiafinal.json" disableOptimization>
-                {(geos, proj) => geos.map((geo, i) => {
-  
-                  const country = data.find(d => d.zones.district === geo.properties.distname)
-                  
-                  return (
-                    <Geography
-                    //   key={geo.properties.distname + i}
-                    //   cacheId={geo.properties.distname + i}
-                      geography={ geo }
-                      projection={ proj }
-                      style={{
-                        default: {
-                        //   fill: country ? customScale(country.val) : "#ECEFF1",
-                        fill: "green",
-                          stroke: "#FFF",
-                          strokeWidth: 0.75,
-                          outline: "none",
-                        },
-                        hover: {
-                          fill: "#263238",
-                          stroke: "#FFF",
-                          strokeWidth: 0.75,
-                          outline: "none",
-                        },
-                        pressed: {
-                          fill: "#263238",
-                          stroke: "#FFF",
-                          strokeWidth: 0.75,
-                          outline: "none",
-                        }
-                      }}
-                    />
-                  )
-                })}
-              </Geographies> */}
-              <Graticule stroke="#EAEAEC" />
-                  <Geographies geography={INDIA_TOPO_JSON} >
-        {({ geographies,projection }) =>
-          geographies.map((geo,i) =>
-          {
-          const country = data.find(d => d.district === geo.properties.district)
+        <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Select Map
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={maped}
+          onChange={handleChange}
           
-          return (
-              
-            <Geography
-              key={geo.Rsmkey}
-              geography={geo}
-              projection={projection}
-              onMouseEnter={() => {
-                const { district, st_nm } = geo.properties;
-                const zone = country.zone;
-                
-                setTooltipContent(`${district} ${st_nm} ${zone}`);
-              }}
-              onMouseLeave={() => {
-                setTooltipContent("");
-              }}
-              style={{
-                default: {
-                //    fill: country.zone === "Green"? "green": "#ECEFF1",
-                fill:  country ? colorScale(country.zone) : "#ECEFF1",
-                   stroke: "#607D8B",
-                   strokeWidth: 0.75,
-                   outline: "none",
-                },
-                hover: {
-                   fill: "#CFD8DC",
-                   stroke: "#607D8B",
-                   strokeWidth: 1,
-                   outline: "none",
-                },
-                pressed: {
-                   fill: "#FF5722",
-                   stroke: "#607D8B",
-                   strokeWidth: 1,
-                   outline: "none",
-                }
-              }}
-            />
-          )})
-        }
-      </Geographies>
-            </ZoomableGroup>
-          </ComposableMap> 
-         
-        </div>
-        <div> <ReactTooltip multiline={true}>{content}</ReactTooltip></div>
+        >
+          <MenuItem value={10}>
+            Zone Map
+          </MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        <FormHelperText>Hover on district/State for details</FormHelperText>
+      </FormControl>
+       <Zone/>
         </>
-      )
-    }
-  
-  
-
-export default Zones;
+    )
+}
---
+++ src/Zones/zone.js
@@ -0,0 +1,169 @@
+import React,{useState} from 'react';
+import ReactTooltip from "react-tooltip";
+import "./styles.css";
+
+// import {Stage} from './Stage';
+// import ZoomContainer from './ZoomContainer';
+// import { geoMercator, geoPath } from "d3-geo";
+//  import {  min, max,scaleLinear} from "d3";
+
+// const world = require("./indiafinal.json")
+// const data = require("./zones.json")
+// console.log(data)
+// var result = arrOfObj.map(function(el) {
+//     var o = Object.assign({}, el);
+//     o.isActive = true;
+//     return o;
+//   })
+// console.log(world.features)
+// function Zones() {
+//     const mercator = geoMercator().scale(800).translate([-800,600])
+//     const project = geoPath().projection(mercator).pointRadius(1);
+//     const minProp = min(world.features, feature => feature.properties.statecode);
+//     const maxProp = max(world.features, feature => feature.properties.statecode);
+//     const colorScale = scaleLinear()
+//       .domain([minProp, maxProp])
+//       .range(["blue", "red"]);
+//     return (
+//         <>
+//         <h2>India Zones</h2>
+//       <Stage width="100%" height="800">
+//         <ZoomContainer>
+//           {world.features.map((feature, index) => {
+//             const d = project(feature)
+//             return (
+//               <path
+//                 key={index}
+//                 d={d}
+//                 fill={colorScale}
+//                 stroke="#666666"
+//                 strokeWidth={0.25}
+//               />
+//             )
+//           })}
+//         </ZoomContainer>
+//       </Stage>
+//       </>
+//     )
+//   }
+import {
+    ComposableMap,
+    ZoomableGroup,
+    Geographies,
+    Geography,
+    Graticule,
+  } from "react-simple-maps"
+  import { scaleLinear,scaleOrdinal } from "d3-scale"
+  
+  const wrapperStyles = {
+    width: "100%",
+    maxWidth: 980,
+    margin: "0 auto",
+  }
+  
+
+ const datax = require("./zones.json")
+ const data =datax.zones
+ const INDIA_TOPO_JSON = require('./indiatop.json');
+
+ 
+
+  const minValue = 5 // based on the data array above
+  const maxValue = 16 // based on the data array above
+  
+  const minColor = "#CFD8DC"
+  const maxColor = "#37474F"
+  
+  const customScale = scaleLinear()
+    .domain([minValue,maxValue])
+    .range([minColor,maxColor])
+
+    const colorScale = scaleOrdinal()
+    .domain(['#D21C1C','#E98305','#2B7336'])
+    .range(['#2B7336','#D21C1C','#E98305']);
+
+
+  
+  function Zone() {
+    const [content, setTooltipContent] = useState("");
+
+    
+      return (
+        <>
+        <h3>Red, Orange and Green zones of India</h3>
+        <div style={wrapperStyles}>
+          <ComposableMap
+            projection="geoMercator"
+            projectionConfig={{
+            //   rotate: [-20.0, -52.0, 0],
+            
+            
+              scale: 1000
+            }}
+            // width={980}
+            // height={651}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            >
+            <ZoomableGroup data-tip="" center={[80, 23]} >
+              <Graticule stroke="#EAEAEC" />
+                  <Geographies geography={INDIA_TOPO_JSON} >
+        {({ geographies,projection }) =>
+          geographies.map((geo,i) =>
+          {
+          const country = data.find(d => d.district === geo.properties.district)
+          
+          return (
+              
+            <Geography
+              key={geo.Rsmkey}
+              geography={geo}
+              projection={projection}
+              onMouseEnter={() => {
+                const { district, st_nm } = geo.properties;
+                const zone = country.zone;
+                
+                setTooltipContent(`${district} (${st_nm}) : ${zone}`);
+              }}
+              onMouseLeave={() => {
+                setTooltipContent("");
+              }}
+              style={{
+                default: {
+                //    fill: country.zone === "Green"? "green": "#ECEFF1",
+                fill:  country ? colorScale(country.zone) : "#ECEFF1",
+                   stroke: "#607D8B",
+                   strokeWidth: 0.75,
+                   outline: "none",
+                },
+                hover: {
+                   fill: "#CFD8DC",
+                   stroke: "#607D8B",
+                   strokeWidth: 1,
+                   outline: "none",
+                },
+                pressed: {
+                   fill: "#FF5722",
+                   stroke: "#607D8B",
+                   strokeWidth: 1,
+                   outline: "none",
+                }
+              }}
+            />
+          )})
+        }
+      </Geographies>
+            </ZoomableGroup>
+          </ComposableMap> 
+         
+        </div>
+        <div> <ReactTooltip multiline={true}>{content}</ReactTooltip></div>
+        </>
+      )
+    }
+  
+  
+
+export default Zone;
