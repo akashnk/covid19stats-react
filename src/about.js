e361e56a64108215fd90ae85f6b6d2428bf704d1
Akash Naik <skydockan@gmail.com>
2020-05-09 03:01:24 +0530

    speeling mistakes fixed

--- src/about.js
+++ src/about.js
@@ -7,8 +7,8 @@ const About = ()=>
         <h4>Motive
 
         </h4><br/>
-The reason for creating this dashboard was to complement the well-known dashboards in the country by providing an unique perpective.tooltip
-Basically I personally had a question like: "What about the trends in different states of India?", "Is the virus spread (growth factor) slowing down?", "How I can compare the recovered/deaths dynamics per-state?", "Which states are doing the proper things to slow down the growth-factor".
+The reason for creating this dashboard was to complement the well-known dashboards in the country by providing an unique perspective.
+Basically I personally had questions like: "What about the trends in different states of India?", "Is the virus spread (growth factor) slowing down?", "How I can compare the recovered/deaths dynamics per-state?", "Which states are doing the proper things to slow down the growth-factor".
 
 The dashboard provides a basic functionality of comparing the charts of different states simultaneously. It also has a world page for country comparison. A maps page is provided for visual comparison of states.
 </p></>)
